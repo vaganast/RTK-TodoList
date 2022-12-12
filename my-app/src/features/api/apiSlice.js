235aa22bbda2cfb8a363fb90e91810be9e4eef93
vaganast <71605687+vaganast@users.noreply.github.com>
2022-12-12 14:52:50 +0200

    Update apiSlice.js

--- my-app/src/features/api/apiSlice.js
+++ my-app/src/features/api/apiSlice.js
@@ -6,12 +6,36 @@ export const apiSlice = createApi({
     endpoints: (builder) => ({
         getTodos: builder.query ({
             query: () => '/todos', //  http get method /todos
+        }),
+        addTodo: builder.mutation ({ //mutation method we change the data
+            query: (todo) => ({ //todo pass in cause means new todo
+                url: '/todos',
+                method: 'POST',
+                body: todo //for the body in that request we pass the new todo
+            })
+        }),
+        updateTodo: builder.mutation ({
+            query: (todo) => ({
+                url: `/todos/${todo.id}`,
+                method: 'PATCH',
+                body: todo
+            })
+        }),
+        deleteTodo: builder.mutation ({
+            query: ({ id }) => ({
+                url: `/todos/${id}`,
+                method: 'DELETE',
+                body: id
+            })
         })
     })
 })
 
-//custom hooks always starts with use and ends with Query 
+//custom hooks always starts with use and ends with Query or Mutation 
 
 export const {
-    useGetTodosQuery
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation
 } = apiSlice
